3d6db01dd23e295f0cba6d6f45c2c4ac7f3420e8
Sova <boquiren40@gmail.com>
2023-03-23 21:20:11 +0800

    latest

--- js/script.js
+++ js/script.js
@@ -23,12 +23,12 @@ const myHobbies = [
     },
     {
         image: 'images/the-walking-dead.jpg',
-        title: 'movies',
+        title: 'watching movies',
         description: 'Watching movies can be a great way to relax, escape reality, and broaden your horizons through exposure to different cultures, perspectives, and ideas.',
     },
     {
         image: 'images/valorant.jpg',
-        title: 'games',
+        title: 'playing games',
         description: 'Playing games can help improve cognitive function, increase social connections, and provide a fun way to relieve stress.',
     }
 ];
